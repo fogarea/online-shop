7b9b3e376a178faa728721c7808cf383ceb04d3f
fogarea <shiitakedd@gmail.com>
2023-06-12 22:24:45 +0300

    refactor: replace favicon plugin

--- webpack/plugins/html.js
+++ webpack/plugins/html.js
@@ -4,6 +4,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin"
 
 export const html = new HtmlWebpackPlugin({
   template: Path("src/index.html"),
+  favicon: Path("src/assets/favicon/favicon.ico"),
   filename: "index.html",
   minify: {
     collapseWhitespace: isProd
